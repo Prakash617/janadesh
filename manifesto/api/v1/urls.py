fb51cd469a9853f902a53dae77a95ab1fdd28439
Prakash <prakashthapa617@gmail.com>
2026-01-14 15:24:26 +0545

    ..

--- manifesto/api/v1/urls.py
+++ manifesto/api/v1/urls.py
@@ -3,5 +3,5 @@ from janadesh.api.router import router
 from .views import ManifestoViewSet
 
 
-router.register(r'manifeso', ManifestoViewSet, basename='manifeso')  # /api/v1/galleries/
+router.register(r'manifesto', ManifestoViewSet, basename='manifeso')  # /api/v1/galleries/
 
